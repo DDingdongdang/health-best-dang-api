9a3eed455424448084318ca0c224fa26396added
twoosky <lhjae3@naver.com>
2023-04-05 23:52:18 +0900

    feat: userDetails 구현체 작성

---
+++ src/main/java/com/healthbest/api/auth/security/UserDetailsVO.java
@@ -0,0 +1,55 @@
+package com.healthbest.api.auth.security;
+
+import com.healthbest.api.user.domain.User;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@AllArgsConstructor
+@Getter
+public class UserDetailsVO implements UserDetails {
+
+    private User user;
+
+    public static UserDetails from(User user) {
+        return new UserDetailsVO(user);
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}

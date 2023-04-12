3851c54757aff722768844f4f476c96827a6eac5
twoosky <lhjae3@naver.com>
2023-04-12 14:49:10 +0900

    refactor: UserDetails 구현체 UserAdapter로 클래스명 수정

--- src/main/java/com/healthbest/api/auth/security/UserAdapter.java
+++ src/main/java/com/healthbest/api/auth/security/UserAdapter.java
@@ -1,26 +1,29 @@
 package com.healthbest.api.auth.security;
 
 import com.healthbest.api.user.domain.User;
+import com.healthbest.api.user.domain.vo.Authority;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import java.util.List;
 
 @AllArgsConstructor
 @Getter
-public class UserDetailsVO implements UserDetails {
+public class UserAdapter implements UserDetails {
 
     private User user;
 
     public static UserDetails from(User user) {
-        return new UserDetailsVO(user);
+        return new UserAdapter(user);
     }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        return List.of(() -> user.getAuthority().toString());
     }
 
     @Override

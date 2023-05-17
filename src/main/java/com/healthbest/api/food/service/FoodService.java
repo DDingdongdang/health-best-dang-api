03bdef3bd5963e11c9b9a7dea3ddbda1f11aafb1
twoosky <lhjae3@naver.com>
2023-05-17 20:29:26 +0900

    feat: 음식 영양 정보 저장 비즈니스 로직 구현

---
+++ src/main/java/com/healthbest/api/food/service/FoodService.java
@@ -0,0 +1,34 @@
+package com.healthbest.api.food.service;
+
+import com.healthbest.api.food.domain.Food;
+import com.healthbest.api.food.dto.FoodInfo;
+import com.healthbest.api.food.repository.FoodRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class FoodService {
+    private final FoodRepository foodRepository;
+
+    @Transactional
+    public void create(FoodInfo foodInfo) {
+        foodInfo.getRow().getInfos().stream()
+                .map(this::generate)
+                .forEach(foodRepository::save);
+    }
+
+    private Food generate(FoodInfo.Info info) {
+        return Food.builder()
+                .name(info.getName())
+                .amount(Integer.parseInt(info.getSize()))
+                .calorie(info.getCalorie())
+                .carbohydrate(info.getCarbohydrate())
+                .protein(info.getProtein())
+                .fat(info.getFat())
+                .sugars(info.getSugars())
+                .sodium(info.getSodium())
+                .build();
+    }
+}

af88d66414ba227c077905123bdab7daab027a39
twoosky <lhjae3@naver.com>
2023-05-17 20:54:03 +0900

    feat: 이름으로 영양정보 조회 쿼리 메서드 작성

--- src/main/java/com/healthbest/api/food/repository/FoodRepository.java
+++ src/main/java/com/healthbest/api/food/repository/FoodRepository.java
@@ -3,5 +3,8 @@ package com.healthbest.api.food.repository;
 import com.healthbest.api.food.domain.Food;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface FoodRepository extends JpaRepository<Food, Long> {
+    Optional<Food> findByName(String name);
 }

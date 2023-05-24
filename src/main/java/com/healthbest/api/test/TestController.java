a00437c63690195d1a2756c0d9fa3f3ffdd6abbd
twoosky <lhjae3@naver.com>
2023-05-24 13:00:11 +0900

    fix: 테스트 dto 수정

--- src/main/java/com/healthbest/api/test/TestController.java
+++ src/main/java/com/healthbest/api/test/TestController.java
@@ -1,8 +1,14 @@
 package com.healthbest.api.test;
 
+import com.healthbest.api.common.dto.TimeDto;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.text.SimpleDateFormat;
+
+import java.time.LocalDateTime;
+import java.util.Date;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -10,6 +16,6 @@ public class TestController {
 
     @GetMapping("/test2")
     public TestResponseDto test() {
-        return new TestResponseDto(1L, "축하축하", "성공이다!!!!!!!!!!!!!!!");
+        return new TestResponseDto(1L, "축하축하", "성공이다!!!!!!!!!!!!!!!", LocalDateTime.now());
     }
 }
--- src/main/java/com/healthbest/api/test/TestResponseDto.java
+++ src/main/java/com/healthbest/api/test/TestResponseDto.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @NoArgsConstructor
 @Getter
 @AllArgsConstructor
@@ -11,4 +13,5 @@ public class TestResponseDto {
     private long id;
     private String title;
     private String content;
+    private LocalDateTime time;
 }

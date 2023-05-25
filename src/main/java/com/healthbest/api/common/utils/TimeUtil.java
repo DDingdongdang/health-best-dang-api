da13aafe98cef7b3d5c83e71d174c4d2136f9b6e
twoosky <lhjae3@naver.com>
2023-05-26 02:29:04 +0900

    refactor: toLocalDateTime 메서드에 유효 시간인지 검사 로직 추가

--- src/main/java/com/healthbest/api/common/utils/TimeUtil.java
+++ src/main/java/com/healthbest/api/common/utils/TimeUtil.java
@@ -9,8 +9,17 @@ public class TimeUtil {
 
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
-
     public static LocalDateTime toLocalDateTime(TimeDto.DateTime dto) {
+        LocalDateTime localDateTime =  getLocalDateTime(dto);
+
+        if (localDateTime.isAfter(LocalDateTime.now())) {
+            throw new RuntimeException("현재 시간보다 미래의 시간 입니다.");
+        }
+
+        return localDateTime;
+    }
+
+    private static LocalDateTime getLocalDateTime(TimeDto.DateTime dto) {
         return LocalDateTime.of(
                 dto.getYear(),
                 dto.getMonth(),
